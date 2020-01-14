9425f722a4559a7569e57f3282ded408573169bf
0xFeeL <bf1995@protonmail.com>
2020-01-13 19:34:16 -0800

    Further layout...

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.sendFile('public/index.html', {root : __dirname});
 });
 
 const port = process.env.PORT || 8080;
